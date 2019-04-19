e01cce5ca3f797289ee97c65624cede1fd4618cf
Rokun Islam <rokuncse8@gmail.com>
2019-04-19 16:26:35 +0600

    Add_Validation_For_Product

--- SbmsWebApplication/SbmsWebApplication/Models/Product.cs
+++ SbmsWebApplication/SbmsWebApplication/Models/Product.cs
@@ -8,16 +8,17 @@ namespace SbmsWebApplication.Models
 {
     public class Product
     {
-        [Required]
+       
         public int Id { get; set; }
   
         [Required]
         public string Code { get; set; }
         [Required]
         public string Name { get; set; }
-        
-        public int CategoryId { get; set; }
+
         [Required]
+        public int CategoryId { get; set; }
+     
         public int ReorderLevel { get; set; }
      
         public string Description { get; set; }
